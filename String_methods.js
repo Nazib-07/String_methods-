0221c65e8edd286c510ab5767f9e6335179f05ef
_@Nazib_07 <nazib9007.ice.bup@gmail.com>
2026-02-07 00:10:17 +0600

    String_methods.js

--- String_methods.js
+++ String_methods.js
@@ -10,3 +10,13 @@ console.log(str2);
 console.log(str2.trim());//remove enpty spaces;
 console.log(str2.trimStart());//remove just start empty space not remove end sapce;
 console.log(str2.trimEnd());//remove just empty empty space not remove start sapce;
+let str3 = "Hello coders!";
+console.log(str3.search("o"));//Search any string;
+console.log(str3.indexOf("!"));//Search index number.
+console.log(str3.lastIndexOf("l"));//Search from last index.
+console.log(str3.startsWith("Meow"));//If given string natch to the str3 string the return true, otherwise return false;
+let human = "Karim Uddin";
+console.log(human.match("din"));//match string to string;
+console.log(human.includes("Udd"));//If match string to string then return true, otherwise return false;
+console.log(human.startsWith("Uddin"));//return false. cos start Karim;
+console.log(human.endsWith("Karim"));//return false. cos end Uddin;
